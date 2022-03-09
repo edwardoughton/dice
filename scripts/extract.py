e8f6c84094e253d8ed976b2b005101f587329788
edwardoughton <edward.oughton@gmail.com>
2022-03-09 09:09:42 -0500

    Add extract script

---
+++ scripts/extract.py
@@ -0,0 +1,142 @@
+"""
+Extract all model data for visualization.
+
+Written by Ed Oughton.
+
+March 2022.
+
+"""
+import os
+import configparser
+import pandas as pd
+import xlwings as xw
+
+CONFIG = configparser.ConfigParser()
+CONFIG.read(os.path.join(os.path.dirname(__file__), '..', 'scripts', 'script_config.ini'))
+BASE_PATH = CONFIG['file_locations']['base_path']
+
+RESULTS = os.path.join(BASE_PATH, '..', 'results')
+
+
+def extract_data():
+    """
+    Function to extract results from DICE workbook.
+
+    """
+    if not os.path.exists(RESULTS):
+        os.makedirs(RESULTS)
+
+    path = os.path.join(BASE_PATH, '..', 'Oughton et al. (2022) DICE.xlsx')
+
+    wb = xw.Book(path)
+
+    extract_total_costs(wb)
+
+    extract_gdp(wb)
+
+    return
+
+
+def extract_total_costs(wb):
+    """
+
+    """
+    sheet = wb.sheets['total_costs']
+
+    data = sheet.range('A1').options(
+        pd.DataFrame,
+        header=1,
+        index=False,
+        expand='table'
+        ).value
+
+    data = data.to_dict('records')
+
+    output = []
+
+    for item in data:
+
+        iso3 = item['ISO3']
+        income = item['Income Group']
+        region = item['Region']
+
+        if str(iso3) == 'nan':
+            continue
+
+        for key, value in item.items():
+
+            if key in ['ISO3','country_name','Total Cost ($)',
+                'Cost Per Pop ($)','Income Group','Region']:
+                continue
+
+            if value == '-':
+                value = 0
+
+            output.append({
+                'iso3': iso3,
+                'decile': key,
+                'cost': float(value),
+                'income': income,
+                'region': region,
+            })
+
+    output = pd.DataFrame(output)
+    path = os.path.join(RESULTS, 'total_cost.csv')
+    output.to_csv(path, index=False)
+
+    return output
+
+
+def extract_gdp(wb):
+    """
+
+    """
+    sheet = wb.sheets['GDP']
+
+    data = sheet.range('A1').options(
+        pd.DataFrame,
+        header=1,
+        index=False,
+        expand='table'
+        ).value
+
+    data = data.to_dict('records')
+
+    output = []
+
+    for item in data:
+
+        iso3 = item['ISO3']
+        income = item['Income Group']
+        region = item['Region']
+
+        if str(iso3) == 'nan':
+            continue
+
+        for key, value in item.items():
+
+            if key in ['ISO3','country_name','Total Cost ($)',
+                'Cost Per Pop ($)','Income Group','Region']:
+                continue
+
+            if value == '-':
+                value = 0
+
+            output.append({
+                'iso3': iso3,
+                'year': key,
+                'gdp': float(value),
+                'income': income,
+                'region': region,
+            })
+
+    output = pd.DataFrame(output)
+    path = os.path.join(RESULTS, 'gdp.csv')
+    output.to_csv(path, index=False)
+
+    return output
+
+
+if __name__ == "__main__":
+
+    extract_data()
